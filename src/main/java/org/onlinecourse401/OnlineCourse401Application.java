3c8937b69e3d18042048f570882ba7649dcb5ea4
StanislavPB <perminovsb@gmail.com>
2024-03-18 21:23:54 +0000

    init commit

---
+++ src/main/java/org/onlinecourse401/OnlineCourse401Application.java
@@ -0,0 +1,10 @@
+package org.onlinecourse401;
+
+
+public class OnlineCourse401Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/test/java/org/onlinecourse401/OnlineCourse401ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.onlinecourse401;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCourse401ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
