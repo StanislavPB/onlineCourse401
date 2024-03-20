0824ee1c9f09e8bd52bfa3696e70e9f285eec402
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-03-20 22:00:11 +0100

    commit1

--- src/main/java/org/onlinecourse401/project/backEnd/service/allServices/CourseService.java
+++ src/main/java/org/onlinecourse401/project/backEnd/service/allServices/CourseService.java
@@ -1,5 +1,5 @@
 package org.onlinecourse401.project.backEnd.service.allServices;
-//add (Request: CourseDto, Response: ClientResponse),
+//addNewCourse (Request: CourseDto, Response: ClientResponse),
 // remove (Request: CourseDto, Response: ClientResponse),
 // find by id (Request: Integer courseId, Response:  ClientResponse)
 public class CourseService {
