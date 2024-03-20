bb42afbed8907dee32659d56999fd00942b98b47
AlinaKlochai <mrs.ali@tuta.io>
2024-03-20 17:48:14 +0100

    test Control

--- src/main/java/org/onlinecourse401/project/backEnd/entity/TestControl.java
+++ src/main/java/org/onlinecourse401/project/backEnd/entity/TestControl.java
@@ -4,10 +4,12 @@ package org.onlinecourse401.project.backEnd.entity;
 import java.util.List;
 
 public class TestControl {
+    private Integer testControlID;
     private String title;
     private List<Question> questions;
 
-    public TestControl(String title, List<Question> questions) {
+    public TestControl(Integer testControlID, String title, List<Question> questions) {
+        this.testControlID = testControlID;
         this.title = title;
         this.questions = questions;
     }
@@ -28,10 +30,19 @@ public class TestControl {
         this.questions = questions;
     }
 
+    public Integer getTestControlID() {
+        return testControlID;
+    }
+
+    public void setTestControlID(Integer testControlID) {
+        this.testControlID = testControlID;
+    }
+
     @Override
     public String toString() {
         return "TestControl{" +
-                "title='" + title + '\'' +
+                "testControlID=" + testControlID +
+                ", title='" + title + '\'' +
                 ", questions=" + questions +
                 '}';
     }
