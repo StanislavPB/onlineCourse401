85c8d209fa799aa6e3073979ccfb85f01d044848
STANISLAV <stachchub@gmail.com>
2024-03-20 15:53:01 +0100

    ox-ox-ox

--- src/main/java/org/onlinecourse401/project/backEnd/repositories/TestResultRepository.java
+++ src/main/java/org/onlinecourse401/project/backEnd/repositories/TestResultRepository.java
@@ -1,4 +1,32 @@
 package org.onlinecourse401.project.backEnd.repositories;
 
-public class TestResultRepository {
+import org.onlinecourse401.project.backEnd.entity.TestResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class TestResultRepository implements TestResultRepositoryInterface{
+    private List<TestResult> testResults;
+
+    public TestResultRepository() {
+        this.testResults = new ArrayList<>();
+    }
+
+    public List<TestResult> getTestResults() {
+        return testResults;
+    }
+
+    @Override
+    public String toString() {
+        return "результат тестирования:" + "Результат теста - " + testResults;
+    }
+
+
+    @Override
+    public List<TestResult> add(TestResult testResult) {
+        List<TestResult> results= new ArrayList<>();
+        results.add(testResult);
+        return results;
+    }
 }
--- src/main/java/org/onlinecourse401/project/backEnd/repositories/TestResultRepositoryInterface.java
+++ src/main/java/org/onlinecourse401/project/backEnd/repositories/TestResultRepositoryInterface.java
@@ -1,4 +1,16 @@
 package org.onlinecourse401.project.backEnd.repositories;
 
+import org.onlinecourse401.project.backEnd.entity.Student;
+import org.onlinecourse401.project.backEnd.entity.TestResult;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface TestResultRepositoryInterface {
+//    public TestResult add(TestResult testResult);
+//    public Optional<TestResult> findById(Integer courseId);
+    public List<TestResult> add(TestResult testResult);
+
+
+
 }
