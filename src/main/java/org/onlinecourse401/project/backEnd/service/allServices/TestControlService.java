351cab00dec93942369faf9d0c75bb955c0e294b
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-03-21 15:16:27 +0100

    commit1

---
+++ src/main/java/org/onlinecourse401/project/backEnd/service/allServices/TestControlService.java
@@ -0,0 +1,36 @@
+package org.onlinecourse401.project.backEnd.service.allServices;
+
+import org.onlinecourse401.project.backEnd.dto.QuestionDto;
+import org.onlinecourse401.project.backEnd.dto.TestControlDto;
+import org.onlinecourse401.project.backEnd.entity.Question;
+import org.onlinecourse401.project.backEnd.entity.TestControl;
+import org.onlinecourse401.project.backEnd.repositories.TestControlRepositoryInterface;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestControlService {
+    private final TestControlRepositoryInterface testControlRepository;
+
+    public TestControlService(TestControlRepositoryInterface testControlRepository) {
+        this.testControlRepository = testControlRepository;
+    }
+    public TestControlDto creatTestControlDto(String title, List<QuestionDto> questionsDto){
+        TestControlDto testControlDto = new TestControlDto(title,questionsDto);
+        return testControlDto;
+    }
+    public List<TestControlDto> createTestControlDtoList(TestControlDto testControlDto){
+        List<TestControlDto> testControlDtoList = new ArrayList<>();
+        testControlDtoList.add(testControlDto);
+        return testControlDtoList;
+    }
+    public List<TestControl> createTestControlList(List<TestControlDto> testControlDtoList){
+        for (TestControlDto testControlDto : testControlDtoList){
+            TestControl newTestControl = new TestControl(null,testControlDto.getTitle(),testControlDto.getQuestionDto());
+           testControlRepository.add(newTestControl);
+            // repository.add(.... в том чмсле - результат работы метода questionService.createQuestionList(testControlDto.getQuestionDto()))
+        }
+        return testControlRepository.findAll();
+    }
+}
+
