00af52f83662a4d8f4a0c7f2b48cbf281eb491e5
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-03-21 17:29:39 +0100

    commit1

--- src/main/java/org/onlinecourse401/project/OnlineCourse401App.java
+++ src/main/java/org/onlinecourse401/project/OnlineCourse401App.java
@@ -4,7 +4,7 @@ import org.onlinecourse401.project.backEnd.repositories.*;
 import org.onlinecourse401.project.backEnd.service.allServices.CourseService;
 import org.onlinecourse401.project.backEnd.service.allServices.QuestionService;
 import org.onlinecourse401.project.backEnd.service.allServices.TestControlService;
-import org.onlinecourse401.project.frontEnd.adminInput.UploadQuestionData;
+import org.onlinecourse401.project.frontEnd.adminInput.UploadCourseData;
 
 public class OnlineCourse401App {
     public static void main(String[] args) {
@@ -14,7 +14,7 @@ public class OnlineCourse401App {
         QuestionService questionService = new QuestionService(questionRepository);
         TestControlService testControlService = new TestControlService(testControlRepository);
         CourseService courseService = new CourseService(courseRepository);
-        UploadQuestionData uploadQuestionData = new UploadQuestionData(courseService, questionService,testControlService);
-        uploadQuestionData.inputQuestionData();
+        UploadCourseData uploadQuestionData = new UploadCourseData(courseService, questionService,testControlService);
+        uploadQuestionData.inputCourseTestQuestionData();
     }
 }
--- src/main/java/org/onlinecourse401/project/frontEnd/adminInput/UploadCourseData.java
+++ src/main/java/org/onlinecourse401/project/frontEnd/adminInput/UploadCourseData.java
@@ -1,76 +1,74 @@
-/*package org.onlinecourse401.project.frontEnd.adminInput;
+package org.onlinecourse401.project.frontEnd.adminInput;
 
-import org.onlinecourse401.project.backEnd.dto.ClientResponseDto;
 import org.onlinecourse401.project.backEnd.dto.CourseDto;
 import org.onlinecourse401.project.backEnd.dto.QuestionDto;
 import org.onlinecourse401.project.backEnd.dto.TestControlDto;
-import org.onlinecourse401.project.frontEnd.util.UserInput;
+import org.onlinecourse401.project.backEnd.entity.Course;
+import org.onlinecourse401.project.backEnd.entity.Question;
+import org.onlinecourse401.project.backEnd.entity.TestControl;
+import org.onlinecourse401.project.backEnd.service.allServices.CourseService;
+import org.onlinecourse401.project.backEnd.service.allServices.QuestionService;
+import org.onlinecourse401.project.backEnd.service.allServices.TestControlService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class UploadCourseData {
+    private final CourseService courseService;
+    private final QuestionService questionService;
+    private final TestControlService testControlService;
 
-    private final CourseService service;
-
-    public UploadCourseData(CourseService service) {
-        this.service = service;
+    public UploadCourseData(CourseService courseService, QuestionService questionService, TestControlService testControlService) {
+        this.courseService = courseService;
+        this.questionService = questionService;
+        this.testControlService = testControlService;
     }
+    public void inputCourseTestQuestionData() {
 
-    public void inputCourseData() {
+        String textQuestion1 = "1. What is the main advantage of using interfaces in Java?";
+        String textOption1Q1 = "0. They allow for multiple inheritance.";
+        String textOption2Q1 = "1. They allow for abstraction and multiple inheritance.";
+        String textOption3Q1 = "2. They allow for abstraction and multiple inheritance.";
+        Integer rightOptionIndexQ1 = 1;
+        String textQuestion2 = "2. What is the purpose of the 'static' keyword in Java?";
+        String textOption1Q2 = "0. To create constants.";
+        String textOption2Q2 = "1. To make a method or variable belong to the class rather than an instance of the class.";
+        String textOption3Q2 = "2. To make a method or variable belong to the class rather than an instance of the class.";
+        Integer rightOptionIndexQ2 = 0;
+        String textQuestion3 = "3. What is the difference between '== and '.equals()' in Java?";
+        String textOption1Q3 = "0. '==' checks for reference equality, while '.equals()' checks for object equality.";
+        String textOption2Q3 = "1. '==' checks for object equality, while '.equals()' checks for reference equality.";
+        String textOption3Q3 = "2. '==' checks for object equality, while '.equals()' checks for reference equality.";
+        Integer rightOptionIndexQ3 = 1;
+        List<String> optionsQ1 = questionService.createOptionsList(textOption1Q1, textOption2Q1, textOption3Q1);
+        QuestionDto questionDto1 = questionService.createQuestionDto(textQuestion1, optionsQ1, rightOptionIndexQ1);
+
+        List<String> optionsQ2 = questionService.createOptionsList(textOption1Q2, textOption2Q2, textOption3Q2);
+        QuestionDto questionDto2 = questionService.createQuestionDto(textQuestion2, optionsQ2, rightOptionIndexQ2);
+
+        List<String> optionsQ3 = questionService.createOptionsList(textOption1Q3, textOption2Q3, textOption3Q3);
+        QuestionDto questionDto3 = questionService.createQuestionDto(textQuestion3, optionsQ3, rightOptionIndexQ3);
+
+        List<QuestionDto> questionDtoList = questionService.createQuestionDtoList(questionDto1, questionDto2, questionDto3);
+        List<Question> questionList = questionService.createQuestionList(questionDtoList);
+
+        System.out.println(questionList);
+
+        String testTitle = "Final Exam";
+        TestControlDto testControlDto1 = testControlService.creatTestControlDto(testTitle,questionDtoList);
+        List<TestControlDto> testControlDtoList =testControlService.createTestControlDtoList(testControlDto1);
+        List<TestControl> testControlList = testControlService.createTestControlList(testControlDtoList,questionList);
+        System.out.println(testControlList);
 
         String courseName = "Java Pro";
         String description = "This course covers advanced Java programming concepts.";
         String content1 = "path/to/content1/file.txt";
         String content2 = "path/to/content2/file.txt";
-        String testTitle = "Final Exam";
-        String textQuestion1 = "1. What is the main advantage of using interfaces in Java?";
-        String textOption1Q1 = "0. They allow for multiple inheritance.";
-        String textOption2Q1 = "1. They allow for abstraction and multiple inheritance.";
-        Integer rightOptionIndexQ1 = 1;
-        String textQuestion2 = "2. What is the purpose of the 'static' keyword in Java?";
-        String textOption1Q2 = "0. To create constants.";
-        String textOption2Q2 = "1. To make a method or variable belong to the class rather than an instance of the class.";
-        Integer rightOptionIndexQ2 = 0;
-        String textQuestion3 = "3. What is the difference between '== and '.equals()' in Java?";
-        String textOption1Q3 = "0. '==' checks for reference equality, while '.equals()' checks for object equality.";
-        String textOption2Q3 = "1. '==' checks for object equality, while '.equals()' checks for reference equality.";
-        Integer rightOptionIndexQ3 = 1;
+        List<String> contents = courseService.createContentList(content1,content2);
+        CourseDto courseDto = courseService.createCourseDto(courseName,description,contents,testControlDtoList);
+        Course course1 = courseService.createNewCourse(courseDto,testControlList);
+        System.out.println(course1);
+
 
     }
+
 }
-/*
-        QuestionDto questionDto1 = new QuestionDto(textQuestion1, Arrays.asList(textOption1Q1,textOption2Q1),rightOptionIndexQ1);
-        QuestionDto questionDto2 = new QuestionDto(textQuestion2,Arrays.asList(textOption1Q2,textOption2Q2),rightOptionIndexQ2);
-        QuestionDto questionDto3 = new QuestionDto(textQuestion3,Arrays.asList(textOption1Q3,textOption2Q3),rightOptionIndexQ3);
-
-        List<QuestionDto> questionDtoList = new ArrayList<>(Arrays.asList(questionDto1,questionDto2,questionDto3));
-
-        TestControlDto testControlDto = new TestControlDto(testTitle,questionDtoList);
-        List<TestControlDto> testControlDtoList = new ArrayList<>();
-
-        CourseDto courseDto = new CourseDto(courseName,description,Arrays.asList(content1,content2),testControlDtoList);
-
-        //ClientResponseDto<Integer> addCourse1 = service.addNewCourse(courseDto);
-        System.out.println(addCourse1);
-    }
-}
-/*
-  String courseName = UserInput.inputText("Please enter course name:");
-        String description = UserInput.inputText("Please enter course description:");
-        String content1 = UserInput.inputText("Please enter path of content:");
-        String content2 = UserInput.inputText("Please enter path of content:");
-        String testTitle = UserInput.inputText("Please enter title of test:");
-        String textQuestion1 = UserInput.inputText("Please enter question text:");
-        String textOption1Q1 = UserInput.inputText("Please enter answer option text:");
-        String textOption2Q1 = UserInput.inputText("Please enter answer option text:");
-        String textQuestion2 = UserInput.inputText("Please enter question text:");
-        String textOption1Q2 = UserInput.inputText("Please enter answer option text:");
-        String textOption2Q2 = UserInput.inputText("Please enter answer option text:");
-        String textQuestion3 = UserInput.inputText("Please enter question text:");
-        String textOption1Q3 = UserInput.inputText("Please enter answer option text:");
-        String textOption2Q3 = UserInput.inputText("Please enter answer option text:");
- */
-
-
