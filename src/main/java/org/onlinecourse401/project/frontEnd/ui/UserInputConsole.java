64ab7f1546ec1c057c0baa0c3fded3cd5cf2126c
AlinaKlochai <mrs.ali@tuta.io>
2024-03-22 17:25:27 +0100

    tes

--- src/main/java/org/onlinecourse401/project/frontEnd/ui/UserInputConsole.java
+++ src/main/java/org/onlinecourse401/project/frontEnd/ui/UserInputConsole.java
@@ -40,4 +40,41 @@ public class UserInputConsole {
         Integer id = ui.inputInteger("Please enter number of chosen course: ");
         return id;
     }
+
+    public void inputRegistrationData() {
+        boolean isValidData = false;
+        do {
+            try {
+                String name1 =  ui.inputText("Please enter your username: ");
+                String email1 = ui.inputText("Please enter your email: ");
+                String password1 = ui.inputText("Please enter your password: ");
+
+                StudentDto studentDto1 = new StudentDto(name1,email1,password1);
+                ClientResponseDto<Student> student1 = studentService.addNewStudent(studentDto1);
+                System.out.println("Response code: "+student1.getResponseCode()+"; Info:"+ student1.getMessage()+ "; : "+ student1.getResponseInfo());
+
+                // Если регистрация прошла успешно, устанавливаем флаг isValidData в true
+                if (student1.getResponseCode() == 200) {
+                    isValidData = true;
+                } else {
+                    // Если данные не прошли валидацию, повторно запрашиваем ввод
+                    System.out.println("Registration failed. Please try again.");
+                }
+            } catch (Exception e) {
+                // Обработка ошибки регистрации
+                System.out.println("An error occurred during registration: " + e.getMessage());
+            }
+        } while (!isValidData);
+
+        // После успешной регистрации запросите выбор курса
+        Integer chosenCourseId = chooseCourseByStudent();
+        // Дальнейшая логика обработки выбранного курса
+    }
+
+    public Integer chooseCourseByStudent() {
+        System.out.println("All courses: ");
+        courseService.printAllCourses();
+        Integer id = ui.inputInteger("Please enter number of chosen course: ");
+        return id;
+    }
 }
