1e6cdc94fc63f9a218e4a3f88197399dc1719e3e
AlinaKlochai <mrs.ali@tuta.io>
2024-03-23 21:02:42 +0100

    tes

--- src/main/java/org/onlinecourse401/project/backEnd/service/allServices/AnalyzeService.java
+++ src/main/java/org/onlinecourse401/project/backEnd/service/allServices/AnalyzeService.java
@@ -3,18 +3,37 @@ package org.onlinecourse401.project.backEnd.service.allServices;
 import org.onlinecourse401.project.backEnd.entity.Student;
 import org.onlinecourse401.project.backEnd.repositories.StudentRepositoryInterface;
 
+import java.security.KeyStore;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 public class AnalyzeService {
     private final StudentRepositoryInterface studentRepository;
-
 
     public AnalyzeService(StudentRepositoryInterface studentRepository) {
         this.studentRepository = studentRepository;
     }
-    private void bestStudentInSchool(){
-       /* projtis po vsem studentam, vybrat studenta s max Grade
 
-        */
 
+    private void bestStudentInSchool() {
+        Optional<Student> bestStudent = studentRepository.findAll().stream()
+                .max(Comparator.comparingInt(student -> {
+                    int maxScore = student.getCourseTestResults().stream()
+                            .flatMap(testResult -> testResult.getCountOfRightAnswers().describeConstable().stream())
+                            .mapToInt(Integer::intValue)
+                            .max()
+                            .orElse(0);
+                    return maxScore;
+                }));
+
+        bestStudent.ifPresent(student -> System.out.println("Best student: StudentId - " + student.getId() + ", Max score: " +
+                student.getCourseTestResults().stream()
+                        .flatMap(testResult -> testResult.getCountOfRightAnswers().describeConstable().stream())
+                        .mapToInt(Integer::intValue)
+                        .max()
+                        .orElse(0)));
     }
     private void testPassRate(){
         // skolko studentov v shkole
