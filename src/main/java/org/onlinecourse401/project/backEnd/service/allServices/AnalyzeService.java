e5f09c7cf30524c44f224a319f5ad1f7bb8d0698
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-03-23 21:29:32 +0100

    commit1

--- src/main/java/org/onlinecourse401/project/backEnd/service/allServices/AnalyzeService.java
+++ src/main/java/org/onlinecourse401/project/backEnd/service/allServices/AnalyzeService.java
@@ -18,28 +18,6 @@ public class AnalyzeService {
         this.studentRepository = studentRepository;
     }
 
-
-    /* public void bestStudentInSchool() {
-        Optional<Student> bestStudent = studentRepository.findAll().stream()
-                .max(Comparator.comparingInt(student -> {
-                    int maxScore = student.getCourseTestResults().stream()
-                            .flatMap(testResult -> testResult.getCountOfRightAnswers().describeConstable().stream())
-                            .mapToInt(Integer::intValue)
-                            .max()
-                            .orElse(0);
-                    return maxScore;
-                }));
-
-        bestStudent.ifPresent(student -> System.out.println("Best student in school: StudentId - " + student.getId() + ", Max score: " +
-                student.getCourseTestResults().stream()
-                        .flatMap(testResult -> testResult.getCountOfRightAnswers().describeConstable().stream())
-                        .mapToInt(Integer::intValue)
-                        .max()
-                        .orElse(0)));
-    }
-
-     */
-
     public void bestStudentsInSchool() {
         Optional<List<Student>> bestStudents = studentRepository.findAll().stream()
                 .collect(Collectors.groupingBy(
@@ -67,10 +45,13 @@ public class AnalyzeService {
                                     .sum()));
         });
     }
-    private void testPassRate(){
+
+    public void countAllStudentsInSchlool() {
         // skolko studentov v shkole
-      //  C1: number of Students na kurse i skolko iz nih proshli test uspeshno, (a esli sdali vse, vyvesti max Ocenku)
-        //  C1: number of Students na kurse i skolko iz nih proshli test uspeshno, (a esli sdali vse, vyvesti max Ocenku)
+    }
+    public void testPassRate(){
+      //  Course1: number of Students na kurse i skolko iz nih proshli test uspeshno, (a esli sdali vse, vyvesti max Ocenku)
+      //  Course2: number of Students na kurse i skolko iz nih proshli test uspeshno, (a esli sdali vse, vyvesti max Ocenku)
 
 
        }
