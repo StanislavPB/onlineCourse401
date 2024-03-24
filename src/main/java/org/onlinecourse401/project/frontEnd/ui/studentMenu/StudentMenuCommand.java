a949f1e75fa35646087952361a61174b6b08805f
EkaterinaBolvakina <ekaterina.bolvakina@gmail.com>
2024-03-24 17:37:36 +0100

    commit1

--- src/main/java/org/onlinecourse401/project/frontEnd/ui/studentMenu/StudentMenuCommand.java
+++
@@ -1,8 +0,0 @@
-package org.onlinecourse401.project.frontEnd.ui.studentMenu;
-
-public interface StudentMenuCommand {
-    void executeCommand();
-
-    String getMenuName();
-    boolean shouldExit();
-}
