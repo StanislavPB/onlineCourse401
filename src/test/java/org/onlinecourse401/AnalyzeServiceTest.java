87c1316b40d4001ed1d19440d36683c12a30a0ad
alex <allexxem@gmail.com>
2024-03-25 11:51:03 +0100

    question

---
+++ src/test/java/org/onlinecourse401/AnalyzeServiceTest.java
@@ -0,0 +1,50 @@
+package org.onlinecourse401;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.onlinecourse401.project.backEnd.entity.Student;
+import org.onlinecourse401.project.backEnd.repositories.StudentRepository;
+import org.onlinecourse401.project.backEnd.repositories.StudentRepositoryInterface;
+import org.onlinecourse401.project.backEnd.service.allServices.AnalyzeService;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+public class AnalyzeServiceTest {
+
+    @Mock
+    private StudentRepositoryInterface studentRepository;
+
+    private AnalyzeService analyzeService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        analyzeService = new AnalyzeService(studentRepository);
+    }
+
+    @Test
+    public void testCountAllStudentsInSchlool() {
+
+        Student student1 = new Student(1, "Alexey Ivanov", "alexey@gmailcom", "alexey1234");
+        Student student2 = new Student(2, "Thomas Mueller", "thomas@gmailcom", "thomas1234");
+        Student student3 = new Student(3, "Oliver Huber", "oliver@gmailcom", "oliver1234");
+        List<Student> students = Arrays.asList(student1, student2, student3);
+
+        when(studentRepository.findAll()).thenReturn(students);
+
+        analyzeService.countAllStudentsInSchlool();
+
+        verify(studentRepository, times(1)).findAll();
+
+    }
+
+}
---
+++ src/test/java/org/onlinecourse401/TestUtils.java
@@ -0,0 +1,22 @@
+package org.onlinecourse401;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+
+public class TestUtils {
+
+    public static String captureSystemOutput(Runnable runnable) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream, true, StandardCharsets.UTF_8);
+        PrintStream originalPrintStream = System.out;
+        System.setOut(printStream);
+
+        try {
+            runnable.run();
+            return byteArrayOutputStream.toString(StandardCharsets.UTF_8);
+        } finally {
+            System.setOut(originalPrintStream);
+        }
+    }
+}
